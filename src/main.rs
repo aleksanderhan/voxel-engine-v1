f7e5c7861fb6d1827eca40e18c76cc081f9c14e0
Aleksander Hansen <aleksanderhan@gmail.com>
2026-02-20 14:00:53 +0100

    Use winit spawn_app on web to avoid control-flow throw

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@ mod chunks;
 mod render;
 mod svo;
 
+#[cfg(target_arch = "wasm32")]
+use winit::platform::web::EventLoopExtWebSys;
+
 fn main() {
     let profile_enabled = std::env::args().any(|arg| arg == "--profile");
     let event_loop = winit::event_loop::EventLoop::new().unwrap();
-    event_loop
-        .run_app(&mut app::App::new(profile_enabled))
-        .unwrap();
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        event_loop.spawn_app(app::App::new(profile_enabled));
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        event_loop
+            .run_app(&mut app::App::new(profile_enabled))
+            .unwrap();
+    }
 }
